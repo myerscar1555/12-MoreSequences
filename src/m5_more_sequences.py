b6ef933240639199078cdae3d81ddd64251893ce
myerscar1555 <myerscl@rose-hulman.edu>
2019-01-21 13:01:04 -0500

    DONE

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -238,9 +238,13 @@ def index_of_first_negative(numbers):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            return k
+    return -1
 
 
 def run_test_contains_an_a():
@@ -296,7 +300,7 @@ def contains_an_a(s):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
@@ -309,6 +313,10 @@ def contains_an_a(s):
     #   Use an explicit loop, as you have done in the other problems.
     #   No fair using the   count   or   find   string methods.
     # -------------------------------------------------------------------------
+    for k in range(len(s)):
+        if s[k] == 'a':
+            return True
+    return False
 
 
 # -----------------------------------------------------------------------------
